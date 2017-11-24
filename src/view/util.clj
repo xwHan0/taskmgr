e52735c4aaae1a18858a48d7e47d1e2746c1a54a
xwHan0 <xwhan2015@outlook.com>
2017-11-24 21:08:20 +0800

    add weekly-report componment

---
+++ src/view/util.clj
@@ -0,0 +1,13 @@
+(ns view.util)
+
+(defn page
+  "把componments填入页面主框架，并调用html函数形成页面。"
+  [jss css & componments]
+  (html5
+    [:head
+      (for [j jss]
+        [:script {:type "text/javascript" :src j}])
+      (for [c css]
+        [:link {:rel "stylesheet" :type "text/css" :href c}])
+      ]
+     (apply vector :body componments)))
