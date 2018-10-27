edd037f8fb6af6160366cc65fd0abf1678d23584
xwHan0 <xwhan2015@outlook.com>
2018-10-27 15:11:59 +0800

    add SQLAlchemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask-sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from gantt import Gantt
 from task import Task, addTask
